eda33cd21e94761fb2fbe74262cbdc6526dd1412
andiniirianaa <andiniriana1@gmail.com>
2025-03-04 13:20:47 +0700

    ade1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\TampilanFrontendController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TampilanFrontendController::class, 'beranda'])->name('beranda');
+Route::get('/profile', [TampilanFrontendController::class, 'profile'])->name('profile');
+Route::get('/program', [TampilanFrontendController::class, 'program'])->name('program');
+Route::get('/layanan', [TampilanFrontendController::class, 'layanan'])->name('layanan');
+Route::get('/produk-jasa', [TampilanFrontendController::class, 'produkJasa'])->name('produkJasa');
+Route::get('/berita', [TampilanFrontendController::class, 'berita'])->name('berita');
+Route::get('/galeri', [TampilanFrontendController::class, 'galeri'])->name('galeri');
+Route::get('/layanan/web', [TampilanFrontendController::class, 'layananWeb'])->name('layanan.Web');
